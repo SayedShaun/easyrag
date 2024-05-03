c3667070ae8eeec9d1fcaaa5d11e6c87948e096b
SayedShaun <sayedshaun4@gmail.com>
2024-05-03 20:40:02 +0600

    hide all method except retriever

--- rag/rag.py
+++ rag/rag.py
@@ -10,24 +10,24 @@ from langchain.chains.conversational_retrieval.base import ConversationalRetriev
 
 class RAG:
     def __init__(
-            self,
+            self, 
             model_id, 
             embedding_model, 
             hf_token, 
-            pdf_path,
-            temperature:float=0.5,
-            max_token:int = 200, 
-        ) -> None:
-        self.model_id = model_id
-        self.temperature = temperature
-        self.embedding_model = embedding_model
-        self.hf_token = hf_token
-        self.pdf_path = pdf_path
-        self.max_token = max_token
-        self.vector_store = self._transform_and_store()
-        self.llm = self._huggingface_llm()
+            pdf_path, 
+            temperature:float=0.5, 
+            max_token:int = 200
+        )->None:
+        self.__model_id = model_id
+        self.__temperature = temperature
+        self.__embedding_model = embedding_model
+        self.__hf_token = hf_token
+        self.__pdf_path = pdf_path
+        self.__max_token = max_token
+        self.__transform_and_store()
+        self.__huggingface_llm()
 
-    def _huggingface_llm(self):
+    def __huggingface_llm(self):
         """
         The function `_huggingface_llm` initializes a Hugging Face pipeline for text generation using a
         specified model and token.
@@ -42,57 +42,65 @@ class RAG:
             bnb_4bit_quant_type="nf4"
         )
         model_config = AutoConfig.from_pretrained(
-            pretrained_model_name_or_path=self.model_id, 
-            token=self.hf_token
+            pretrained_model_name_or_path=self.__model_id, 
+            token=self.__hf_token
         )
         model = AutoModelForCausalLM.from_pretrained(
-            pretrained_model_name_or_path = self.model_id,
-            token=self.hf_token,
+            pretrained_model_name_or_path = self.__model_id,
+            token=self.__hf_token,
             config=model_config,
             quantization_config=bnb_config,
             device_map="auto",
             trust_remote_code=True
         )
         tokenizer = AutoTokenizer.from_pretrained(
-            pretrained_model_name_or_path=self.model_id,
-            token=self.hf_token
+            pretrained_model_name_or_path=self.__model_id,
+            token=self.__hf_token
         )
         hf_pipeline = pipeline(
             model = model,
             task="text-generation",
             tokenizer = tokenizer,
             return_full_text = True,
-            temperature=0.5,
-            max_new_tokens=self.max_token,
+            temperature=self.__temperature,
+            max_new_tokens=self.__max_token,
             repetition_penalty=1
         )
         llm = HuggingFacePipeline(pipeline=hf_pipeline)
         return llm
 
-    def _pdf_loader(self, pdf_path):
+    def __pdf_loader(self, pdf_path):
         """
         The function `_pdf_loader` reads text content from a PDF file using the `PdfReader` class and
         returns the concatenated text from all pages.
         
-        :param pdf_path: The `pdf_path` parameter in the `_pdf_loader` function is a string that
-        represents the file path to the PDF file that you want to load and extract text from. This
-        function reads the PDF file using the `PdfReader` class and extracts text content from each page
-        in the PDF file
-        :return: The `_pdf_loader` function returns a string containing the extracted text from all
-        pages of the PDF located at the specified `pdf_path`.
+        :param pdf_path: The `_pdf_loader` function you provided seems to be a method for loading text
+        content from a PDF file using the `PyPDF2` library. The `pdf_path` parameter is the file path to
+        the PDF file that you want to extract text from
+        :return: The `_pdf_loader` function returns the concatenated raw text extracted from the pages
+        of the PDF file located at the specified `pdf_path`.
         """
         raw_texts = ""
-        pdf_reader = PdfReader(pdf_path)
-        for page in pdf_reader.pages:
-            content = page.extract_text()
-            if content:
-                raw_texts += content
+        pdf_reader = None
+        try:
+            pdf_reader = PdfReader(pdf_path)
+            for page in pdf_reader.pages:
+                content = page.extract_text()
+                if content:
+                    raw_texts += content
+        except Exception as e:
+            print("An error occurred while processing the PDF:", e)
+        finally:
+            if pdf_reader is not None:
+                pdf_reader.close()
+
         return raw_texts
+
     
     def _weblink_loader(self):
         pass
 
-    def _transform_and_store(self):
+    def __transform_and_store(self):
         """
         The function `_transform_and_store` processes text data from a PDF file, splits it into chunks,
         embeds the chunks using a Hugging Face model on a CUDA device, and stores the resulting vectors
@@ -102,19 +110,10 @@ class RAG:
         splitting and chunking using RecursiveCharacterTextSplitter. The method then creates a vector
         store using FAISS from the chunked texts and returns this vector store.
         """
-        embedding = HuggingFaceEmbeddings(
-            model_name=self.embedding_model, 
-            model_kwargs={"device":"cuda"}
-        )
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000, chunk_overlap=20
-        )
-        chunk_texts = text_splitter.split_text(
-            self._pdf_loader(self.pdf_path)
-        )
-        vector_store = FAISS.from_texts(
-            chunk_texts, embedding=embedding
-        )
+        embedding = HuggingFaceEmbeddings(model_name=self.__embedding_model, model_kwargs={"device":"cuda"})
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+        chunk_texts = text_splitter.split_text(self.__pdf_loader(self.__pdf_path))
+        vector_store = FAISS.from_texts(chunk_texts, embedding=embedding)
         return vector_store
     
     def retrieve_answer(self, query):
@@ -127,8 +126,8 @@ class RAG:
         each line of the final part of the answer
         """
         chain = ConversationalRetrievalChain.from_llm(
-            self.llm, 
-            self.vector_store.as_retriever(), 
+            self.__huggingface_llm, 
+            self.__transform_and_store.as_retriever(), 
             return_source_documents=True
         )
 
@@ -142,3 +141,7 @@ class RAG:
         for x in text_store[-1].split("\n"):
             print(x)
 
+
+rag = RAG()
+
+rag.
