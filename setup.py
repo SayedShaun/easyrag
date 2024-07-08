266b24edcf95040d14f0dd018f417aaf1cb6e029
SayedShaun <sayedshaun4@gmail.com>
2024-07-08 20:13:17 +0600

    updated

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
+import subprocess
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+
+def get_version_from_git():
+    try:
+        version = subprocess.check_output(['git', 'describe', '--tags']).strip().decode('utf-8')
+        if version.startswith('v'):
+            version = version[1:]  # Remove the 'v' prefix if it exists
+        return version
+    except Exception as e:
+        print(f"Error getting version from Git: {e}")
+        return '0.1.0'  # Default version if Git command fails
+
+
+VERSION = get_version_from_git() 
 DESCRIPTION = 'OpenRAG Python Package'
 
 setup(
@@ -21,5 +34,8 @@ setup(
         "bitsandbytes",
         "accelerate"
     ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires=">=3.10",
 )
 
