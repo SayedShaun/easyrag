39c3251ffa5f8fc749d0a179576d1da0a2c904d2
SayedShaun <sayedshaun4@gmail.com>
2024-07-09 12:42:05 +0600

    updated

--- easyrag/models.py
+++ easyrag/models.py
@@ -4,7 +4,7 @@ import sys
 import torch
 from typing import Tuple
 from pydantic import SecretStr
-from easyrag.utils import (
+from easyrag._helper_function import (
     pdf_loader, 
     store_user_chat_history, 
     transform_and_store, 
@@ -36,7 +36,11 @@ class HuggingFaceModel:
             hf_token: SecretStr = None,
             embedding_model: str = "sentence-transformers/all-MiniLM-L6-v2",
             temperature: float = 0.5,
-            max_token: int = 1000
+            max_token: int = 1000,
+            trust_remote_code: bool = False,
+            repetition_penalty: float = 1,
+            return_full_text: bool = False,
+            do_sample: bool = True
         ) -> None:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         if device.type == "cuda":
@@ -45,7 +49,11 @@ class HuggingFaceModel:
                 embedding_model,
                 temperature,
                 max_token,
-                hf_token
+                hf_token,
+                trust_remote_code,
+                repetition_penalty,
+                return_full_text,
+                do_sample
             )
         else:
             raise RuntimeError(
@@ -61,38 +69,13 @@ class HuggingFaceModel:
             embedding_model: str,
             temperature: float,
             max_token: int,
-            hf_token: SecretStr
+            hf_token: SecretStr,
+            trust_remote_code: bool,
+            repetition_penalty: float,
+            return_full_text: bool,
+            do_sample: bool
         ) -> Tuple:
-        """
-        The function `__huggingface_llm` initializes a Hugging Face model for text generation with
-        specified configurations and returns the model and tokenizer.
-        
-        :param model_id: The `model_id` parameter in the function `__huggingface_llm` is used to specify
-        the identifier or name of the pre-trained language model that you want to load for text
-        generation. This could be the name of a specific model like "gpt2" or a custom model identifier
-        :type model_id: str
-        :param embedding_model: The `embedding_model` parameter refers to the name or identifier of the
-        Hugging Face model that will be used for generating embeddings. This model will be responsible
-        for converting input text into numerical representations that capture the semantic meaning of
-        the text
-        :type embedding_model: str
-        :param temperature: The `temperature` parameter in the function `__huggingface_llm` is used in
-        text generation models to control the randomness of the generated text. A higher temperature
-        value will result in more diverse and creative outputs, while a lower temperature value will
-        produce more conservative and predictable outputs. It essentially scales
-        :type temperature: float
-        :param max_token: The `max_token` parameter in the function `__huggingface_llm` specifies the
-        maximum number of tokens that the text generation model will produce. This parameter controls
-        the length of the generated text output
-        :type max_token: int
-        :param hf_token: The `hf_token` parameter in the function `__huggingface_llm` is used as a token
-        for the Hugging Face model. It is typically a string that represents the specific token or model
-        configuration you want to use. This token is used when loading the model and tokenizer from the
-        H
-        :type hf_token: str
-        :return: A tuple containing an instance of the HuggingFacePipeline (`llm`) and an instance of
-        the HuggingFaceEmbeddings (`embedding`) is being returned.
-        """
+  
         bnb_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_compute_dtype=torch.float16,
@@ -124,12 +107,12 @@ class HuggingFaceModel:
             model=model,
             task="text-generation",
             tokenizer=tokenizer,
-            return_full_text=True,
+            return_full_text=return_full_text,
             temperature=temperature,
             max_new_tokens=max_token,
-            repetition_penalty=1,
-            trust_remote_code=True,
-            do_sample=True
+            repetition_penalty=repetition_penalty,
+            trust_remote_code=trust_remote_code,
+            do_sample=do_sample
         )
         
         llm = HuggingFacePipeline(pipeline=hf_pipeline)
@@ -173,18 +156,7 @@ class GoogleGemini:
             temperature: float,
             max_token: int
         ) -> Tuple:
-        """
-        The function `__google_gen_ai` initializes instances of GoogleGenerativeAI and
-        GoogleGenerativeAIEmbeddings using the provided API key.
-        
-        :param api_key: The `api_key` parameter is a string that represents the API key required to
-        access the Google Generative AI services. This key is used for authentication and authorization
-        purposes when making requests to the Google Generative AI API
-        :type api_key: str
-        :return: A tuple containing two instances: `llm` which is an instance of the GoogleGenerativeAI
-        class with the model "gemini-pro" and `embedding` which is an instance of the
-        GoogleGenerativeAIEmbeddings class with the model "models/embedding-001".
-        """
+
         llm = GoogleGenerativeAI(
             model="gemini-pro",
             google_api_key=api_key,
@@ -213,18 +185,7 @@ class OpenAI:
         self._vector_store = transform_and_store(raw_texts, embedding)
 
     def _openai(self, api_key: SecretStr, temperature: float) -> Tuple:
-        """
-        The function `__openai` takes an API key as input and returns instances of the ChatOpenAI and
-        OpenAIEmbeddings classes initialized with the provided API key.
-        
-        :param api_key: The `api_key` parameter is a string that represents the API key required to
-        access the OpenAI services. This key is used for authentication and authorization purposes when
-        making requests to the OpenAI API
-        :type api_key: str
-        :return: A tuple containing two objects: a ChatOpenAI model initialized with the "gpt-3.5-turbo"
-        model and a Google API key, and an OpenAIEmbeddings model initialized with the
-        "text-embedding-ada-002" model and a Google API key.
-        """
+
         llm = ChatOpenAI(model="gpt-3.5-turbo", google_api_key=api_key, temperature=temperature)
         embedding = OpenAIEmbeddings(
             model="text-embedding-ada-002", google_api_key=api_key
